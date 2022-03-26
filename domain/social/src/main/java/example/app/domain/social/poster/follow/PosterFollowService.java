d0dd98eb90514977edf0aab67515a74062e84b3d
chiayen <chiayenchien@gmail.com>
2022-03-27 01:05:23 +0800

    refactor(poster/follow): move use case logic to facade

--- domain/social/src/main/java/example/app/domain/social/poster/follow/PosterFollowService.java
+++ domain/social/src/main/java/example/app/domain/social/poster/follow/PosterFollowService.java
@@ -1,7 +1,5 @@
 package example.app.domain.social.poster.follow;
 
-import example.app.domain.social.poster.IPosterVo;
-import example.app.domain.social.poster.PosterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
@@ -9,14 +7,12 @@ import org.springframework.validation.annotation.Validated;
 import javax.validation.constraints.NotNull;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Validated
 @Service
 public class PosterFollowService {
     private final PosterFollowRepository posterFollowRepository;
-    private final PosterService posterService;
 
     public List<IPosterFollowVo> findByPosterId(@NotNull Long posterId) {
         return Collections.unmodifiableList(posterFollowRepository.findByPosterId(posterId));
@@ -24,31 +20,5 @@ public class PosterFollowService {
 
     public List<IPosterFollowVo> findByFollowedBy(@NotNull Long posterId) {
         return Collections.unmodifiableList(posterFollowRepository.findByFollowedBy(posterId));
-    }
-
-    public List<IPosterVo> findFollowerByPosterId(@NotNull Long posterId) {
-        final var followerIds = this.findByPosterId(posterId)
-                .stream()
-                .map(IPosterFollowVo::getFollowedBy)
-                .collect(Collectors.toSet());
-
-        if (followerIds.isEmpty()) {
-            return List.of();
-        }
-
-        return posterService.findByIds(followerIds);
-    }
-
-    public List<IPosterVo> findFollowedByPosterId(@NotNull Long posterId) {
-        final var followedIds = this.findByFollowedBy(posterId)
-                .stream()
-                .map(IPosterFollowVo::getPosterId)
-                .collect(Collectors.toSet());
-
-        if (followedIds.isEmpty()) {
-            return List.of();
-        }
-
-        return posterService.findByIds(followedIds);
     }
 }
--- service/platform/src/main/java/example/app/platform/social/application/facade/PosterFollowFacade.java
+++ service/platform/src/main/java/example/app/platform/social/application/facade/PosterFollowFacade.java
@@ -1,11 +1,13 @@
 package example.app.platform.social.application.facade;
 
+import example.app.domain.social.poster.follow.IPosterFollowVo;
 import example.app.domain.social.poster.follow.PosterFollowService;
 import example.app.domain.social.poster.follow.command.PosterFollowCommandService;
 import example.app.platform.social.application.dto.output.PosterDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,19 +16,32 @@ import java.util.stream.Collectors;
 public class PosterFollowFacade {
     private final PosterFollowService posterFollowService;
     private final PosterFollowCommandService posterFollowCommandService;
+    private final PosterFacade posterFacade;
 
-    public List<PosterDto> findFollower(Long id) {
-        return posterFollowService.findFollowerByPosterId(id)
+    public List<PosterDto> findFollower(@NotNull Long posterId) {
+        final var followerIds = posterFollowService.findByPosterId(posterId)
                 .stream()
-                .map(PosterDto::new)
-                .collect(Collectors.toList());
+                .map(IPosterFollowVo::getFollowedBy)
+                .collect(Collectors.toSet());
+
+        if (followerIds.isEmpty()) {
+            return List.of();
+        }
+
+        return posterFacade.findByIds(followerIds);
     }
 
-    public List<PosterDto> findFollowed(Long id) {
-        return posterFollowService.findFollowedByPosterId(id)
+    public List<PosterDto> findFollowed(@NotNull Long posterId) {
+        final var followedIds = posterFollowService.findByFollowedBy(posterId)
                 .stream()
-                .map(PosterDto::new)
-                .collect(Collectors.toList());
+                .map(IPosterFollowVo::getPosterId)
+                .collect(Collectors.toSet());
+
+        if (followedIds.isEmpty()) {
+            return List.of();
+        }
+
+        return posterFacade.findByIds(followedIds);
     }
 
     public void follow(Long posterId, Long followedBy) {
