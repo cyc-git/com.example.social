c5bcac4b42bbc9aa59d2c33b3677d3d10175f045
chiayen <chiayenchien@gmail.com>
2022-03-27 04:31:56 +0800

    chore(user): remove no longer needed method

--- service/platform/src/main/java/example/app/platform/user/application/facade/UserAuthFacade.java
+++ service/platform/src/main/java/example/app/platform/user/application/facade/UserAuthFacade.java
@@ -5,9 +5,6 @@ import example.app.platform.user.application.dto.output.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
-
 @RequiredArgsConstructor
 @Service
 public class UserAuthFacade {
@@ -15,11 +12,5 @@ public class UserAuthFacade {
 
     public UserDto login(String account, String password) {
         return UserDto.from(userAuthService.authenticate(account, password));
-    }
-
-    public void logout(HttpServletResponse response) {
-        final var cookie = new Cookie("userAccount", "");
-        cookie.setMaxAge(0);
-        response.addCookie(cookie);
     }
 }
