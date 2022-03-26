41eaaab5627c84d3207308a9375909bfd80bbbe5
chiayen <chiayenchien@gmail.com>
2022-03-26 18:39:34 +0800

    feat(i18n): add utility method of I18nAsserts

--- common/core/src/main/java/example/app/common/core/i18n/I18nAsserts.java
+++ common/core/src/main/java/example/app/common/core/i18n/I18nAsserts.java
@@ -6,14 +6,20 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class I18nAsserts {
 
-    void notNull(Object object, String i18nKey) {
+    public void notNull(Object object, String i18nKey) {
         if (object == null) {
             throw new I18nIllegalArgumentException(i18nKey);
         }
     }
 
-    void isTrue(boolean expression, String i18nKey) {
+    public void isTrue(boolean expression, String i18nKey) {
         if (!expression) {
+            throw new I18nIllegalArgumentException(i18nKey);
+        }
+    }
+
+    public void isFalse(boolean expression, String i18nKey) {
+        if (expression) {
             throw new I18nIllegalArgumentException(i18nKey);
         }
     }
