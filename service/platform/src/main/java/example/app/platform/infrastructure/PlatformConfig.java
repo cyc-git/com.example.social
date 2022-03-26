c4cb11c3d53d26fa415f3cfac6de4908dec7d77e
chiayen <chiayenchien@gmail.com>
2022-03-27 03:58:43 +0800

    feat(platform): enable caching

--- service/platform/src/main/java/example/app/platform/infrastructure/PlatformConfig.java
+++ service/platform/src/main/java/example/app/platform/infrastructure/PlatformConfig.java
@@ -4,12 +4,14 @@ import example.app.core.data.jooq.IncludeDataJooq;
 import example.app.core.event.IncludeEvent;
 import example.app.core.system.IncludeSystem;
 import example.app.common.web.security.IncludeCommonSecurity;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 
 @IncludeCommonSecurity
 @IncludeDataJooq
 @IncludeEvent
 @IncludeSystem
+@EnableCaching
 @Configuration
 public class PlatformConfig {
 }
