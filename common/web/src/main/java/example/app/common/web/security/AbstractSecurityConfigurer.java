f832cb29130111fa713273e3add22d2eef8eee32
chiayen <chiayenchien@gmail.com>
2022-03-27 14:28:14 +0800

    refactor(security): rearrange security config

--- common/web/src/main/java/example/app/common/web/security/AbstractSecurityConfigurer.java
+++ common/web/src/main/java/example/app/common/web/security/AbstractSecurityConfigurer.java
@@ -1,5 +1,6 @@
 package example.app.common.web.security;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,6 +25,12 @@ public abstract class AbstractSecurityConfigurer extends WebSecurityConfigurerAd
                 .formLogin()
                 .disable()
                 .logout()
+                .disable()
+                .csrf()
                 .disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     }
 }
--- service/platform/src/main/java/example/app/platform/infrastructure/security/PlatformSecurityConfigurer.java
+++ service/platform/src/main/java/example/app/platform/infrastructure/security/PlatformSecurityConfigurer.java
@@ -1,26 +1,22 @@
 package example.app.platform.infrastructure.security;
 
+import example.app.common.web.security.AbstractSecurityConfigurer;
 import example.app.domain.social.poster.PosterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
-public class PlatformSecurityConfigurer extends WebSecurityConfigurerAdapter {
+public class PlatformSecurityConfigurer extends AbstractSecurityConfigurer {
     private final PosterService posterService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
         http.authorizeRequests()
                 .antMatchers(
-                        "/swagger-ui.html",
-                        "/swagger-ui/**",
-                        "/v3/api-docs",
-                        "/v3/api-docs/**",
                         "/user/auth/login**",
                         "/user/register**"
                 )
@@ -30,15 +26,6 @@ public class PlatformSecurityConfigurer extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .addFilterBefore(new SimpleUserAuthenticateFilter(posterService), UsernamePasswordAuthenticationFilter.class)
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .formLogin()
-                .disable()
-                .logout()
-                .disable()
-                .csrf()
-                .disable();
+                .addFilterBefore(new SimpleUserAuthenticateFilter(posterService), UsernamePasswordAuthenticationFilter.class);
     }
 }
